2a9105a16d1ff6b4ade46df9905728015e6779ca
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-10-14 08:28:05 -0300

    automatted commit

--- game.rb
+++ game.rb
@@ -1,27 +1,26 @@
 OPTIONS = ["Pedra", "Papel", "Tesoura"]
 COMP_CHOICE = ["Pedra", "Papel", "Tesoura"].sample
 
-def check_result(player_choice, tries)
-    
-    if COMP_CHOICE == player_choice
-        puts "Você fez a mesma escolha q a maquina, EMPATE!"
-        exit
-    else
-        if COMP_CHOICE == "Pedra"
-        player_win = player_choice == "Papel"
-        elsif COMP_CHOICE == "Papel"
-        player_win = player_choice == "Tesoura"
-        elsif COMP_CHOICE == "Tesoura"
-        player_win = player_choice == "Pedra"
-        end
+def check_result(player_choice, tries)  
+  if COMP_CHOICE == player_choice
+    puts "Você fez a mesma escolha q a maquina, EMPATE!"
+    exit
+  else
+    if COMP_CHOICE == "Pedra"
+      player_win = player_choice == "Papel"
+    elsif COMP_CHOICE == "Papel"
+      player_win = player_choice == "Tesoura"
+    elsif COMP_CHOICE == "Tesoura"
+      player_win = player_choice == "Pedra"
     end
+  end
 
-    if player_win
-        puts "Você venceu! Computador escolheu #{COMP_CHOICE} em #{tries} tentativas"
-        exit
-    else
-        puts "Você perdeu! Computador escolheu #{COMP_CHOICE}"
-    end
+  if player_win
+    puts "Você venceu! Computador escolheu #{COMP_CHOICE} em #{tries} tentativas"
+    exit
+  else
+    puts "Você perdeu! Computador escolheu #{COMP_CHOICE}"
+  end
 end
 
 tries = 1
